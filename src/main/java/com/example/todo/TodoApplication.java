d4fc9ea84a6108f62f0ca596a69bb42a2981dc5e
silvervolt1234 <105256888+silvervolt1234@users.noreply.github.com>
2025-09-26 20:30:51 +0900

    Initial commit

---
+++ src/main/java/com/example/todo/TodoApplication.java
@@ -0,0 +1,25 @@
+package com.example.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+/*
+ * SpringBootServletInitializer
+ * - war 파일을 배포할 경우 해당 클래스를 반드시 상속
+ * - 외부 톰캣에서 동작하기 위해 web.xml에 반드시 애플리케이션 컨텍스트 등록
+ * 아파치 톰캣이 구동시 web.xml을 읽어 웹 애플리케이션 구동
+ */
+@SpringBootApplication
+public class TodoApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return super.configure(builder);
+	}
+
+}
